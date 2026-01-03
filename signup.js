08a566eb6b78042e2d5a023a0333363eaaa34172
Yuyaaa <111202415957@mhs.dinus.ac.id>
2026-01-03 18:04:40 +0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,28 @@
+/*=Hash PW=*/
+function hashPassword(oassword){
+    return btoa(password);
+}
+/*=Register=*/
+function registerUser(nama, email, password){
+    const user = {
+        nama: nama,
+        email: email,
+        password: hashPassword(password)
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+}
+/*=Login=*/
+function loginUSer(email, password){
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (!user) return false;
+
+    return ( email == user.email && hashPassword(password) == user.password);
+}
+/*=Session=*/
+function setSession(){
+    sessionStorage.setItem("isLogin", "true");
+}
+function logout(){
+    sessionStorage.removeItem("isLogin");
+    window.location.href = "login.html";
+}
